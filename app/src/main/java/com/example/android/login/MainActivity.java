3d5a49d321bc35dd8292591d674c3c092f76f9c0
“icychains24” <ranade.siddhesh@gmail.com>
2017-07-14 16:02:32 +0530

    New front page created - Siddhesh

--- app/src/main/java/com/example/android/login/MainActivity.java
+++ app/src/main/java/com/example/android/login/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.android.login;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,58 +11,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button login_user=(Button) findViewById(R.id.loginu);
-        login_user.setOnClickListener(new View.OnClickListener(){
-       @Override
-            public void onClick(View view)
-            {
-                Intent userloginintent=new Intent(MainActivity.this,UserLogin.class);
-                startActivity(userloginintent);
 
-
-            }
-        });
-
-
-        Button login_admin=(Button) findViewById(R.id.logina);
-        login_admin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                Intent adminloginintent=new Intent(MainActivity.this,AdminLogin.class);
-                startActivity(adminloginintent);
-
-
-            }
-        });
-
-
-        Button signUpAdmin =(Button) findViewById(R.id.sign_up_admin);
-        signUpAdmin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                Intent signUpAdminIntent =new Intent(MainActivity.this,signUpAsAdmin.class);
-                startActivity(signUpAdminIntent);
-
-
-            }
-        });
-
-        Button signUpUser =(Button) findViewById(R.id.sign_up_user);
-        signUpUser.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                Intent signUpUserIntent=new Intent(MainActivity.this,signUpAsUser.class);
-                startActivity(signUpUserIntent);
-
-
-            }
-        });
-
-
-
+//        Button login_user=(Button) findViewById(R.id.loginu);
+//        login_user.setOnClickListener(new View.OnClickListener(){
+//       @Override
+//            public void onClick(View view)
+//            {
+//                Intent userloginintent=new Intent(MainActivity.this,UserLogin.class);
+//                startActivity(userloginintent);
+//
+//
+//            }
+//        });
+//
+//
+//        Button login_admin=(Button) findViewById(R.id.logina);
+//        login_admin.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Intent adminloginintent=new Intent(MainActivity.this,AdminLogin.class);
+//                startActivity(adminloginintent);
+//
+//
+//            }
+//        });
+//
+//
+//        Button signUpAdmin =(Button) findViewById(R.id.sign_up_admin);
+//        signUpAdmin.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Intent signUpAdminIntent =new Intent(MainActivity.this,signUpAsAdmin.class);
+//                startActivity(signUpAdminIntent);
+//
+//
+//            }
+//        });
+//
+//        Button signUpUser =(Button) findViewById(R.id.sign_up_user);
+//        signUpUser.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Intent signUpUserIntent=new Intent(MainActivity.this,signUpAsUser.class);
+//                startActivity(signUpUserIntent);
+//
+//
+//            }
+//        });
 
     }
+
+    public void userSignIn(View view) {
+        //nothing yet
+    }
+
+
 }
