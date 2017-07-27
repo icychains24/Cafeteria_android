0e95de37a8e4b52327e07f4bfa6760cc7e8c1912
“icychains24” <ranade.siddhesh@gmail.com>
2017-07-27 23:10:42 +0530

    Firebase - adding users and admin

--- app/src/main/java/com/example/android/login/AdminLogin.java
+++ app/src/main/java/com/example/android/login/AdminLogin.java
@@ -1,11 +1,20 @@
 package com.example.android.login;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 /**
  * Created by USER on 6/28/2017.
@@ -13,21 +22,21 @@ import android.widget.EditText;
 
 public class AdminLogin extends AppCompatActivity {
 
-    String adminMobile;
-    String adminPin;
+    EditText phoneNumber;
+    private DatabaseReference mDatabase;
+    private DatabaseReference countDatabase;
+    private Button mFirebaseUserBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adminlogin);
 
+        mFirebaseUserBtn = (Button) findViewById(R.id.userConfirmButtonActivityMain);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        EditText phoneNumber = (EditText) findViewById(R.id.phoneNumberEnterTextAdminLogin);
-        adminMobile = phoneNumber.getText().toString();
-
-        EditText pin = (EditText) findViewById(R.id.pinAdminLogin);
-        adminPin = pin.getText().toString();
 
 
         //When admin clicks the login button
@@ -36,12 +45,29 @@ public class AdminLogin extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //if(adminPin == "0000") {
-                Intent intent = new Intent(AdminLogin.this, AdminUpdateMenu.class);
-                startActivity(intent);
-                //}
-                //else
-                    //Toast.makeText(view.getContext(), "Wrong PIN entered!", Toast.LENGTH_SHORT).show();*/
+                //When admin enters phone numbers
+                phoneNumber = (EditText) findViewById(R.id.phoneNumberEnterTextAdminLogin);
+                String adminMobile = phoneNumber.getText().toString();
+
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Mobile", adminMobile);
+                dataMap.put("Name", "I am admin!");
+                mDatabase.child("ADMIN").push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Log.v("MainActivity", "if task is successful");
+                            Toast.makeText(AdminLogin.this,"Data Stored Successfully", Toast.LENGTH_LONG).show();
+
+                        }
+                        else{
+                            Log.v("MainActivity", "if task is NOT successful");
+                            Toast.makeText(AdminLogin.this,"Data Not Stored", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
 
             }
 
--- app/src/main/java/com/example/android/login/MainActivity.java
+++ app/src/main/java/com/example/android/login/MainActivity.java
@@ -2,30 +2,84 @@ package com.example.android.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import static com.example.android.login.R.id.userMobileNumberInputActivityMain;
+import static com.example.android.login.R.id.userNameInputActivityMain;
 
 public class MainActivity extends AppCompatActivity {
 
-    String phoneNumber;
+    //String phoneNumber;
+    //String name;
+    private DatabaseReference mDatabase;
+    private DatabaseReference countDatabase;
+    private Button mFirebaseUserBtn;
+    EditText userName;
+    EditText userPhoneNumber;
+    Button userConfirm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //When user enters the phone number
-        EditText userPhoneNumber = (EditText) findViewById(R.id.userMobileNumberInputActivityMain);
-        phoneNumber = userPhoneNumber.getText().toString();
+        mFirebaseUserBtn = (Button) findViewById(R.id.userConfirmButtonActivityMain);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         //When user clicks the Confirm button
-        Button userConfirm = (Button) findViewById(R.id.userConfirmButtonActivityMain);
+        userConfirm = (Button) findViewById(R.id.userConfirmButtonActivityMain);
         userConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //When user enters name
+                userName = (EditText) findViewById(userNameInputActivityMain);
+                String  name = userName.getText().toString();
+
+                //When user enters the phone number
+                userPhoneNumber = (EditText) findViewById(userMobileNumberInputActivityMain);
+                String phoneNumber = userPhoneNumber.getText().toString();
+
+                //Part 1 - Saving to Database code here
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Mobile",phoneNumber);
+                dataMap.put("Name",name);
+                mDatabase.child("USERS").push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Log.v("MainActivity", "if task is successful");
+                            Toast.makeText(MainActivity.this,"Data Stored Successfully", Toast.LENGTH_LONG).show();
+                            userPhoneNumber.setText("");
+                            userName.setText("");
+                        }
+                        else{
+                            Log.v("MainActivity", "if task is NOT successful");
+                            Toast.makeText(MainActivity.this,"Data Not Stored", Toast.LENGTH_LONG).show();
+                            userPhoneNumber.setText("");
+                            userName.setText("");
+                        }
+                    }
+                });
+
+
+                //Part 2 - OTP code here
                 Intent intent = new Intent(MainActivity.this, OTP.class);
                 startActivity(intent);
 
@@ -35,14 +89,16 @@ public class MainActivity extends AppCompatActivity {
         //when admin clicks the 'if admin, click here' text at the bototm
         TextView ifAdmin = (TextView) findViewById(R.id.ifAdminClickHereTextActivityMain);
         ifAdmin.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(MainActivity.this, AdminLogin.class);
-                startActivity(intent2);
+
+                Intent intent = new Intent(MainActivity.this, AdminLogin.class);
+                startActivity(intent);
             }
         });
 
 
-    } //main()
+    }//main()
 
 } //class MainActivity
